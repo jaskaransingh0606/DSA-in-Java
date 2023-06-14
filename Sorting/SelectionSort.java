65523441c42f8dd0ca5bbbaa62b8e9612de93094
jaskaransingh0606 <sardarshinesingh12@gmail.com>
2023-06-14 22:15:25 +0530

    selection sort

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,36 @@
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = { 4, 10, 1, 6, 3, 7, 8, 2 };
+        selection(arr);
+    }
+
+    static void selection(int[] arr) {
+        for (int i = 0; i < arr.length; i++)
+         {
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+            swap(arr, maxIndex, last);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    static int getMaxIndex(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
+}
