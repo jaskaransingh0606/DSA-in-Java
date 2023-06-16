200c61aeafb4cb63093523fc9226de04751c8751
jaskaransingh0606 <sardarshinesingh12@gmail.com>
2023-06-16 22:15:08 +0530

    Constructor

---
+++ Constructor/Construc.java
@@ -0,0 +1,37 @@
+ class Construc1 {
+    private String name;
+    private int age;
+
+    public Construc1(String name,int age){
+        this.name=name;
+        this.age=age;
+    }
+    
+         
+    public int getAge() 
+    {
+        return age;
+    }
+    public String getName()
+     {
+        return name;
+    }
+
+
+    public void show() {
+        System.out.println(age + " " + name);
+    }  
+   
+}
+
+public class Construc {
+    public static void main(String[] args) {
+        Construc1 obj = new Construc1 ("Rahul",20);
+       
+        obj.show();
+       
+       
+
+    }
+
+}
