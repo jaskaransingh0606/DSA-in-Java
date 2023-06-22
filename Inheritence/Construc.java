ddec74df46bf573e1e7e8730a363a961bbab4f8f
jaskaransingh0606 <sardarshinesingh12@gmail.com>
2023-06-22 18:22:54 +0530

    constructor in Inheritence

---
+++ Inheritence/Construc.java
@@ -0,0 +1,37 @@
+class Demo1{
+      public Demo1(){
+        //Super();                                                  //Called by default in every constructor
+        System.out.println("Parent class Constructor");
+
+    }
+     public Demo1(int x,int y){
+        System.out.println("Parameterized parent class constructor");
+
+    }
+}
+
+class Demo2 extends Demo1{
+      public Demo2(){
+        System.out.println("child class Constructor");
+
+    }
+     public Demo2(int x,int y){
+        System.out.println("Parameterized child class constructor");
+
+    }
+}
+
+
+
+
+
+
+
+
+public class Construc {
+    public static void main(String[] args) {
+        Demo2 obj=new Demo2();
+        
+        
+    }
+}
